666dd1fc694b252879d669d6a25b533ca24a58dd
Maneesh Murali <103819019+amnman@users.noreply.github.com>
2026-05-07 01:07:29 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+    Map<Character, Integer> map1 = new HashMap<>();
+
+    if (s1.length() > s2.length()) {
+        return false;
+    }
+
+    for (char c : s1.toCharArray()) {
+        map1.put(c, map1.getOrDefault(c, 0) + 1);
+    }
+
+    int l = 0, r = 0, n = s2.length();
+    Map<Character, Integer> map2 = new HashMap<>();
+
+    while (r < n) {
+        char c = s2.charAt(r);
+
+        if(!map1.containsKey(c)){
+            map2.clear();
+            r++;
+            l=r;
+            continue;
+        }
+
+        map2.put(c, map2.getOrDefault(c, 0) + 1);
+
+        while (map2.get(c) > map1.get(c)) {
+            char leftChar = s2.charAt(l); // not r
+            map2.put(leftChar, map2.get(leftChar) - 1);
+            if (map2.get(leftChar) == 0) {
+                map2.remove(leftChar);
+            }
+            l++;
+        }
+
+        if (r - l + 1 == s1.length()) {
+            return true;
+        }
+
+        r++;
+    }
+
+    return false;
+}
+}
